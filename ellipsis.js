25223d2313de39ab489e35772a383737fbbedf0e
paul pham <paul@aquaron.com>
2012-02-24 00:16:33 -0800

    v1.0

---
+++ ellipsis.js
@@ -0,0 +1,29 @@
+$.fn.ellipsis = function() {
+	return this.each(function() {
+		var _orig_h = $(this).height(),
+			_orig_ws = $(this).css('whiteSpace'),
+			_str = $(this).html(),
+			_max, _i;
+
+		$(this).html('&nbsp;').css({
+			height: 'auto',
+			display: 'block',
+			overflow: 'hidden'
+		});
+
+		_max = Math.floor(_orig_h / $(this).height()) * $(this).height();
+
+		for (_i = 1; _i <= _str.length; _i++) {
+			$(this).html(_str.substring(0, _i) + '&hellip;');
+			if ($(this).height() > _max) {
+				$(this).html(_str.substring(0, _i - 1) + '&hellip;');
+				break;
+			} else {
+				$(this).html(_str.substring(0, _i));
+			}
+		}
+
+		$(this).height(_orig_h);
+		return true;
+	});
+};
